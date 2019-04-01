2e012e014cf500baa21a6631e46d10c578b699fc
Lasmyr <ldedullantes@globe.com.ph>
2019-04-01 16:27:06 +0800

    Added make matrix function and cache inverse

---
+++ ProgrammingAssignment2_cacheinversematrix.R
@@ -0,0 +1,25 @@
+makeMatrix <- function(x = numeric()) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) s <<- inverse
+  getinverse <- function() s
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheInverse <- function(x, ...) {
+  s <- x$getinverse()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setinverse(s)
+  s
+}
